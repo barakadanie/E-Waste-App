a63a226f2f0605fa1cbd2b2e3f6af07833a08c73
barakadanie <74895402+barakadanie@users.noreply.github.com>
2022-06-08 07:33:24 +0300

    Second Commit

--- app/src/main/java/com/baraka/appe_waste/ProfileActivity.java
+++ app/src/main/java/com/baraka/appe_waste/ProfileActivity.java
@@ -62,7 +62,7 @@ public class ProfileActivity extends AppCompatActivity {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(ProfileActivity.this,MainActivity.class);
+                Intent intent=new Intent(ProfileActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
